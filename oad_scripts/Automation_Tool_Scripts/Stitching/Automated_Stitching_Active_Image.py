c8309c817f37a28a149d02b322699ddadb84332d
sebi06 <sebi06@users.noreply.github.com>
2017-05-19 15:31:51 +0200

    Add files via upload

---
+++ oad_scripts/Automation_Tool_Scripts/Stitching/Automated_Stitching_Active_Image.py
@@ -0,0 +1,12 @@
+﻿# Run Stitching automatically after acquisition
+# Script by Sven Terclavers - Sven.Terclavers@zeiss.com
+
+from System.IO import File, Directory, FileInfo, Path
+
+image = Zen.Application.Documents.ActiveDocument
+
+Stitchset = r'Stitching_Channel_1.czips'
+functionsetting1 = Zen.Processing.Transformation.Geometric.Stitching(image)
+functionsetting1.Load(Stitchset)
+
+# End
